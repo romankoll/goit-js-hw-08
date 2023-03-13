26fa84aa7e61b7f8877791d76e67bd4926a788ca
Roman Kolobych <romankol1223@gmail.com>
2023-03-13 23:28:52 +0200

    перепровірка з ГітДесктопу

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const gallerySectionEll = document.querySelector('.gallery');
+
+function createGallary(obj) {
+  return obj.map(
+    ell => `
+      <a class="gallery__item" href="${ell.original}">
+        <img
+          class="gallery__image"
+          src="${ell.preview}"
+          alt="${ell.description}"
+        />
+      </a>`
+  );
+}
+
+const addGalleryItemsList = createGallary(galleryItems).slice(',').join(' ');
+
+gallerySectionEll.insertAdjacentHTML('beforeend', addGalleryItemsList);
+
+/* options */
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+  overlayOpacity: 0.9,
+  howCounter: true,
+});
+
+function handleImgClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+gallerySectionEll.addEventListener('click', handleImgClick);
