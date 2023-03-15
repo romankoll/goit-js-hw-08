e6bc81e09662a975fe5f250bf3b5d732c28be28f
Roman Kolobych <romankol1223@gmail.com>
2023-03-15 19:43:06 +0200

    2 дз

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoEl = document.querySelector('#vimeo-player');
+
+const player = new Player(videoEl, {});
+
+player.on('timeupdate', throttle(getTime, 1000));
+
+const videoCurrentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(videoCurrentTime);
+
+function getTime(ell) {
+  localStorage.setItem('videoplayer-current-time', ell.seconds);
+}
