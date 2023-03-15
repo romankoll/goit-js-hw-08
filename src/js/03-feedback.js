d2d842c2b299576dd3bf88368f3395c34d33ba6f
Roman Kolobych <romankol1223@gmail.com>
2023-03-15 20:40:58 +0200

    start task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
+import throttle from 'lodash.throttle';
+const formEl = document.querySelector(form);
 
+const feedbackMessageTextarea = form.querySelector('textarea');
+const feedbackEmailInput = form.querySelector('input');
+
+const feedbackForm = 'feedback-form-state';
