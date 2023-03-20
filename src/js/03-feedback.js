85e55324cf273ec4a861a011e1c373c11f6e4de8
Roman Kolobych <romankol1223@gmail.com>
2023-03-20 19:18:35 +0200

    3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,38 @@
 import throttle from 'lodash.throttle';
-const formEl = document.querySelector(form);
+
+const form = document.querySelector('.feedback-form');
 
 const feedbackMessageTextarea = form.querySelector('textarea');
 const feedbackEmailInput = form.querySelector('input');
 
-const feedbackForm = 'feedback-form-state';
+const FEEDBACK_FORM = 'feedback-form-state';
+
+populateTextarea();
+
+form.addEventListener('submit', handleFormSubmit);
+form.addEventListener('input', throttle(handleFormInput, 1000));
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(FEEDBACK_FORM)));
+  evt.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_FORM);
+}
+
+function handleFormInput() {
+  const formMessage = {
+    email: feedbackEmailInput.value,
+    message: feedbackMessageTextarea.value,
+  };
+
+  localStorage.setItem(FEEDBACK_FORM, JSON.stringify(formMessage));
+}
+
+function populateTextarea() {
+  const savedFormMessage = localStorage.getItem(FEEDBACK_FORM);
+  if (savedFormMessage) {
+    const parsedSavedMessage = JSON.parse(savedFormMessage);
+    feedbackEmailInput.value = parsedSavedMessage.email;
+    feedbackMessageTextarea.value = parsedSavedMessage.message;
+  }
+}
